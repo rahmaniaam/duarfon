8b6441f4137d2038a44994b1a9e9885b2b35556d
rahmaniaam <rahmaniaam26@gmail.com>
2018-11-29 23:31:25 +0700

    working upload

--- server/index.js
+++ server/index.js
@@ -1,4 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer  = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'ml/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, 'csvFile.csv')
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'text/csv') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
@@ -23,6 +50,10 @@ if (cluster.isMaster) {
 
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  
+  app.use('ml/', express.static('ml'))
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
 
   // Answer API requests.
   app.get('/api', function (req, res) {
@@ -38,4 +69,17 @@ if (cluster.isMaster) {
   app.listen(PORT, function () {
     console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
   });
+
+  // DuarfOn API handlers
+  app.post('/upload', upload.single('csvFile'), (req, res, next) => {
+    const uploadFile = req.file;
+    const fileName = req.file.filename;
+    
+    
+    res.json({
+      msg: fileName
+    })
+    console.log(uploadFile)
+  })
 }
+
